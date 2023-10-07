c77d91a8ba0dff7344ae10b68ea8148b211f0977
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-10-07 14:54:37 -0600

    [style]: Refactored CSS to SCSS
    This commit also removes old "styles.css" file.
    
    Signed-off-by: Abraham Jimenez <abraham.jimenez.romo@gmail.com>

--- routes/static.js
+++ routes/static.js
@@ -4,7 +4,7 @@ const router = express.Router();
 // Static Routes
 // Set up "public" folder / subfolders for static files
 router.use(express.static("public"));
-router.use("/css", express.static(__dirname + "public/css"));
+router.use("/scss", express.static(__dirname + "public/scss"));
 router.use("/js", express.static(__dirname + "public/js"));
 router.use("/images", express.static(__dirname + "public/images"));
 
