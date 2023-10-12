4fcc5bd0634f1d4fd84f1155f947e38b6b2faa8f
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-10-11 20:21:07 -0600

    server.js File
    Signed-off-by: Abraham Jimenez <abraham.jimenez.romo@gmail.com>

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const env = require('dotenv').config();
 const app = express();
 const static = require('./routes/static');
 const baseController = require("./controllers/baseController");
+const inventoryRoute = require("./routes/inventoryRoute");
 
 /* ***********************
  * View Engine and Templates
@@ -27,6 +28,9 @@ app.use(static);
 
 // Index Route
 app.get('/', baseController.buildHome);
+
+// Inventory Routes
+app.use("/inv", inventoryRoute);
 
 /* ***********************
  * Local Server Information
