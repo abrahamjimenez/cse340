1a2728fba5074dbf39769509ad41e04acc0717e2
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-10-19 18:48:08 -0600

    Assignment 3 - Task 2
    Signed-off-by: Abraham Jimenez <abraham.jimenez.romo@gmail.com>

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -2,11 +2,12 @@
 const express = require("express");
 const router = new express.Router();
 const invController = require("../controllers/invController");
+const Utilities = require("../utilities/index")
 
 // Route to build inventory by classification view
-router.get("/type/:classificationId", invController.buildByClassificationId);
+router.get("/type/:classificationId",Utilities.handleErrors(invController.buildByClassificationId));
 
 // Route to build specific inventory item detail
-router.get("/detail/:invId", invController.buildDetailedView); //todo: invId might break
+router.get("/detail/:invId", Utilities.handleErrors(invController.buildDetailedView));
 
 module.exports = router;
