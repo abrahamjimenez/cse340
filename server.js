7a4354303e0b6b3be4b47321196d1e95ab7a5d9c
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-10-23 15:41:21 -0600

    Require the Session package and DB connection
    Signed-off-by: Abraham Jimenez <abraham.jimenez.romo@gmail.com>

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ const static = require('./routes/static');
 const baseController = require("./controllers/baseController");
 const inventoryRoute = require("./routes/inventoryRoute");
 const utilities = require("./utilities/index");
+const session = require("express-session");
+const pool = require('./database/');
 
 /* ***********************
  * View Engine and Templates
