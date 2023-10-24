e594f9c44bb8d443b99d82b374460432ceb0276c
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-10-23 20:32:33 -0600

    Add flash message to 'login.ejs'
    - This commit also adds layout for form in the account/login page
    - Create '_login.scss'
    
    Signed-off-by: Abraham Jimenez <abraham.jimenez.romo@gmail.com>

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -5,6 +5,7 @@ const utilities = require("../utilities/");
 * *************************************** */
 async function buildLogin(req, res, next) {
 	let nav = await utilities.getNav();
+	req.flash("notice", "This is a flash message.")
 	res.render("account/login", {
 		title: "Login",
 		nav,
