ad24b8619e4a97e9d808e1bd8d0d2c2b07c9edb8
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-10-24 17:28:16 -0600

    Deliver register view
    Signed-off-by: Abraham Jimenez <abraham.jimenez.romo@gmail.com>

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -5,11 +5,23 @@ const utilities = require("../utilities/");
 * *************************************** */
 async function buildLogin(req, res, next) {
 	let nav = await utilities.getNav();
-	req.flash("notice", "This is a flash message.")
+	req.flash("notice", "This is a flash message.");
 	res.render("account/login", {
 		title: "Login",
 		nav,
 	});
 }
 
-module.exports = {buildLogin};
+/* ****************************************
+*  Deliver register view
+* *************************************** */
+async function buildRegister(req, res, next) {
+	let nav = await utilities.getNav();
+	req.flash("notice", "This is a flash message.");
+	res.render("account/register", {
+		title: "Register",
+		nav,
+	});
+}
+
+module.exports = {buildLogin, buildRegister};
