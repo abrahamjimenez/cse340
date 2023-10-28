73ee3691989a83d5c0ff359959e90f26326185b7
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-10-28 11:53:30 -0600

    Server-Side Data Validation | Register View
    Signed-off-by: Abraham Jimenez <abraham.jimenez.romo@gmail.com>

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,14 +2,20 @@ const express = require("express");
 const router = new express.Router();
 const utilities = require("../utilities/");
 const accountsController = require("../controllers/accountController");
+const regValidate = require("../utilities/account-validation");
 
 // Route to build login page in account
 router.get("/login", utilities.handleErrors(accountsController.buildLogin));
 
 // Route to build register page in account
-router.get("/register", utilities.handleErrors(accountsController.buildRegister))
+router.get("/register", utilities.handleErrors(accountsController.buildRegister));
 
-// Enable the registration route
-router.post("/register", utilities.handleErrors(accountsController.registerAccount))
+// Process the registration data
+router.post(
+	"/register",
+	regValidate.registrationRules(),
+	regValidate.checkRegData,
+	utilities.handleErrors(accountsController.registerAccount)
+);
 
 module.exports = router;
